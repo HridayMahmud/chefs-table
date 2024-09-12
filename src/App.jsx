2777e9bbb95d868509eb67f48d53e9224028318c
HRIDAY MAHMUD <hriday.mahmud200@gmail.com>
2024-09-12 14:10:45 +0600

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Header/Header'
+function App() {
+
+
+  return (
+    <>
+    <div className='w-[1220px] mx-auto py-12 '>
+      <div>
+      <Header></Header>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import search from './../assets/Images/search.png'
+import profileicon from './../assets/Images/profileicon.png'
+
+const Header = () => {
+  return (
+    <div>
+     <div className='header flex justify-center gap-[170px] items-center'>
+      <div className="recipi-container  w-[250px]">
+      <h1 className='text-3xl  font-bold'>Recipe Calories</h1>
+      </div>
+      <div className='ancor flex gap-6'>
+       <a href="#">Home</a>
+       <a href="#">Recipes</a>
+       <a href="#">About</a>
+       <a href="#">Seacrch</a>
+      </div>
+     <div className="searchandprofile-container flex gap-2 justify-center  items-center">
+     <div className=" search w-64 bg-[#150B2B0D] rounded-[50px] py-2 px-9 ">
+              <div className=" flex gap-2 items-center">
+              <img className=' w-[18px] h-[18px] ' src={search} alt="" />
+             <input className='  w- outline-0 w-[180px] bg-transparent ' type="search" name="" id="" placeholder='search' />
+
+              </div>
+      </div>
+      <img className='w-12' src={profileicon} alt="" />
+     </div>
+     </div>
+    </div>
+  )
+}
+
+export default Header
