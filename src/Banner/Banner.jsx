38831401e93397a757c3f4c157619159b025308c
HRIDAY MAHMUD <hriday.mahmud200@gmail.com>
2024-09-17 16:22:51 +0600

    bg-cover-done

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='banner-container px-48 mb-24 py-32 text-center'>
+    <div className='banner-container bg-custom-image bg-cover bg-center h-64 w-full px-48 mb-24 py-32 text-center'>
       <div className="banner-text-container  text-white  ">
       <h1 className='text-[52px] font-bold mb-6'>Discover an exceptional cooking class tailored for you!</h1>
       <p className='text-[18px] font-normal mb-10'>A chef is a culinary professional who is skilled in preparing, cooking, and presenting food. They often specialize in a particular type of cuisine or cooking technique. Chefs are responsible for creating menus, selecting ingredients, and ensuring the quality of dishes</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{svelte,js,ts,jsx,tsx}",
+   
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-image': "url('D:\ReactAll\CHEFS-TABLE\chefs-table\src\assets\Images\beef-chaomi.jpg')",
+       
+      },
+    },
   },
   plugins: [],
 }
